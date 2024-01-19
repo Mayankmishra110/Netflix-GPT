5f4d6fc42dfca379c1ecab2f723cc0546c7dfa48
Mayankmishra110 <kmmay15@gmail.com>
2024-01-19 17:04:54 +0530

    Building browse page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,11 +1,23 @@
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
+      {/*
+         MainContainer
+          - VideoBackground
+          - videoTitle
+         SecondaryContainer
+          - MovieList * n
+          - cards * n
+  */}
     </div>
   );
 };
